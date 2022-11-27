342e3174a06e788879dc370ebdd14453fcb91708
secondfirst <he110myn0pe77@gmail.com>
2022-11-27 20:53:05 +0900

    add build settings.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ctrlpanel
 ctrlpanel
+
+# note
+## build
+1.build config
+file > project structure > Artifact > new Config
+
+manifest path ---> resources
+
+2.build 
+build > Build Artifact > (artifact name) > clean and build.
+
+
+## run 
+
+run jarfiles by following command.
+```
+java -cp ctrlpanel.main.jar org.example.Main
+```
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,11 @@ package org.example;
 
 import org.example.control.MainListener;
 
-import java.awt.*;
-
 public class Main {
     public static void main(String[] args) {
 
         println("== started == ");
         var main = new MainListener();
-        main.setSize(new Dimension(600, 400));
         main.setVisible(true);
         main.setLocation(300,300);
 
